a6efc8ba1f70245e5d699212c49bcd26c899f0b6
NghiaHD <nghialmao@gmail.com>
2024-10-09 10:50:32 +0700

    chore: Delete WebCompiler

--- WebCompiler/WebCompiler/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebCompiler.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
