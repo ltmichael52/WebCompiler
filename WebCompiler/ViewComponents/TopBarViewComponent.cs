0d336bd3cb313e6809388b1d861bf131f0daee1b
PhongTranNewbie <emphan1975@gmail.com>
2024-10-12 16:11:44 +0700

    style: fix UI, upload file text

---
+++ WebCompiler/ViewComponents/TopBarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCompiler.ViewComponents
+{
+    public class TopBarViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
