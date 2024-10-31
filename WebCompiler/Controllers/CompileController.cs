dc2e80882bc1e7b3b4c91f74c0f5bcee556184c0
NghiaHD <nghialmao@gmail.com>
2024-10-31 09:41:55 +0700

    refactor : python compiler

---
+++ WebCompiler/Controllers/CompileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCompiler.Controllers
+{
+    public class CompileController : Controller
+    {
+
+        /// <summary>
+        /// Python Compiler
+        /// </summary>
+        [HttpGet("python")]
+        public IActionResult Python()
+        {
+            ViewBag.language = "python";
+            return View();
+        }
+
+    }
+}
