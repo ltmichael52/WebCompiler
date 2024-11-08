c81c140247c9c2ada82481e3c7347002d73d04ae
Thinute <tongthanhthao2009@gmail.com>
2024-11-08 09:48:21 +0700

    Python compiler ( sprint 1 ) 14/10

---
+++ WebCompiler/Controllers/PythonCompilerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebCompiler.Models;
+
+namespace WebCompiler.Controllers
+{
+    namespace PythonWebCompiler.Controllers
+    {
+        public class PythonCompilerController : Controller
+        {
+            [HttpGet]
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Compile(PythonCompiler codeInput)
+            {
+                if (codeInput == null || string.IsNullOrEmpty(codeInput.Code))
+                {
+                    return View("Index", new PythonCompiler());
+                }
+
+                string result = CompilePythonCode(codeInput.Code);
+
+                return View("Index", new PythonCompiler
+                {
+                    Code = codeInput.Code,
+                    Output = result
+                });
+            }
+
+
+
+            private string CompilePythonCode(string code)
+            {
+                string result = string.Empty;
+
+                string filePath = Path.Combine(Path.GetTempPath(), "temp_code.py");
+                System.IO.File.WriteAllText(filePath, code);
+
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = "python",
+                    Arguments = filePath,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+
+                using (var process = Process.Start(startInfo))
+                {
+                    result = process.StandardOutput.ReadToEnd() + process.StandardError.ReadToEnd();
+                    process.WaitForExit();
+                }
+
+                return result;
+
+            }
+        }
+    }
+}
---
+++ WebCompiler/Models/PythonCompiler.cs
@@ -0,0 +1,8 @@
+﻿namespace WebCompiler.Models
+{
+    public class PythonCompiler
+    {
+        public string Code { get; set; }
+        public string Output { get; set; } 
+    }
+}
