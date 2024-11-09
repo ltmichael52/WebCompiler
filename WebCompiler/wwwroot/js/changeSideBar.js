b41ae0f11f91ec154b9c1f26230e0820e5306344
LeQuan128 <lequan0949213634@gmail.com>
2024-11-09 17:52:15 +0700

    feat: ISSUE-301:Home Index

---
+++ WebCompiler/wwwroot/js/changeSideBar.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener('load', function () {
+    const currentUrl = window.location.href;
+    const buttons = document.querySelectorAll('.change-lang-btn');
+
+    buttons.forEach(button => {
+        if (currentUrl === button.getAttribute('href')) {  
+            button.classList.add('active');
+        } else {
+            button.classList.remove('active');
+        }
+    });
+});
